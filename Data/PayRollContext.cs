ef14ba850c54d8fee9f75543a34e621aa864b4ee
Gustavo Estrada <Gustavo Estrada@DESKTOP-Q0R9E6V>
2024-06-18 18:53:17 -0600

    Agregar archivos de proyecto.

---
+++ Data/PayRollContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedModels;
+
+namespace Nomina_Api.Data
+{
+    public class PayRollContext: DbContext
+    {
+        public PayRollContext(DbContextOptions<PayRollContext> options): base(options)
+        { 
+        }
+        public DbSet<PayRoll> payRolls { get; set; }
+        public DbSet<Employees> employees { get; set; }
+        public DbSet<Incomes> incomes { get; set; }
+        public DbSet<Deductions> deductions { get; set; }
+    }
+}
